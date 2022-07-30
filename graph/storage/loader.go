5bc9031a174676bbb51d594bb4a33a8cc8b1555d
nishisuke <ryoskns+git@gmail.com>
2022-07-30 16:18:45 +0900

    fix loader

--- graph/storage/loader.go
+++ graph/storage/loader.go
@@ -5,43 +5,44 @@ import (
 	"context"
 	"errors"
 	"example/graph/db"
-	"fmt"
 
 	"github.com/graph-gophers/dataloader"
-	"gorm.io/gorm"
 )
 
 type (
-	Foo struct {
-		Conn func(context.Context) *gorm.DB
-	}
 	IGetUsers interface {
-		GetUsers(context.Context, dataloader.Keys) []*dataloader.Result
+		GetUsers(context.Context, []string) (map[string]db.User, error)
 	}
 )
 
-func NewFoo(con func(context.Context) *gorm.DB) *Foo {
-	return &Foo{con}
-}
-
 func NewUsersLoader(l IGetUsers) *dataloader.Loader {
-	return dataloader.NewBatchedLoader(l.GetUsers)
+	u := func(ctx context.Context, keys dataloader.Keys) []*dataloader.Result {
+		um, err := l.GetUsers(ctx, conv(keys))
+
+		if err != nil {
+			return []*dataloader.Result{{Error: err}}
+		}
+
+		return packResult(keys, um)
+	}
+
+	return dataloader.NewBatchedLoader(u)
 }
 
-func (l *Foo) GetUsers(ctx context.Context, keys dataloader.Keys) []*dataloader.Result {
-	con := l.Conn(ctx)
-	um, err := aa(con, keys)
-
-	if err != nil {
-		return []*dataloader.Result{
-			{Error: err},
-		}
+func conv(keys dataloader.Keys) []string {
+	ids := make([]string, len(keys))
+	for i, k := range keys {
+		ids[i] = k.String()
 	}
+	return ids
+}
+
+func packResult[T any](keys dataloader.Keys, look map[string]T) []*dataloader.Result {
 
 	res := make([]*dataloader.Result, len(keys))
 
 	for i, key := range keys {
-		if u, ok := um[key.String()]; ok {
+		if u, ok := look[key.String()]; ok {
 			res[i] = &dataloader.Result{Data: u}
 		} else {
 			res[i] = &dataloader.Result{Error: errors.New("NotFound")}
@@ -49,19 +50,4 @@ func (l *Foo) GetUsers(ctx context.Context, keys dataloader.Keys) []*dataloader.
 	}
 
 	return res
-}
-
-func aa(d *gorm.DB, keys dataloader.Keys) (map[string]db.User, error) {
-	var users []db.User
-	if err := d.Find(&users, keys).Error; err != nil {
-		return nil, err
-	}
-
-	res := make(map[string]db.User)
-
-	for _, u := range users {
-		res[fmt.Sprintf("%d", u.ID)] = u
-	}
-
-	return res, nil
 }
