7d58c6e3e7e889e5552ad951ff8aecb54a4eedb1
nishisuke <ryoskns+git@gmail.com>
2022-08-01 22:35:41 +0900

    init

--- go.mod
+++ go.mod
@@ -4,22 +4,12 @@ go 1.18
 
 require (
 	github.com/99designs/gqlgen v0.17.13
-	github.com/graph-gophers/dataloader v5.0.0+incompatible
 	github.com/vektah/gqlparser/v2 v2.4.6
-	gorm.io/driver/mysql v1.3.5
-	gorm.io/gorm v1.23.8
 )
 
 require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/mitchellh/mapstructure v1.3.1 // indirect
-	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.8.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
--- server.go
+++ server.go
@@ -1,20 +1,14 @@
 package main
 
 import (
-	"context"
 	"example/graph"
-	"example/graph/app"
-	"example/graph/db"
 	"example/graph/generated"
-	"example/graph/repos"
-	"example/graph/storage"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"gorm.io/gorm"
 )
 
 const defaultPort = "8080"
@@ -25,33 +19,11 @@ func main() {
 		port = defaultPort
 	}
 
-	// DB
-	con, err := db.Setup()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	con.AutoMigrate(&db.User{})
-
-	// Graph
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{App: app.NewApp()}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", Middleware(con, srv))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
-
-func NewLoader(con *gorm.DB) *storage.Loader {
-	useRepo := repos.NewUserRepo(func(ctx context.Context) *gorm.DB {
-		return con.Debug().WithContext(ctx)
-	})
-	return storage.NewLoader(useRepo)
-}
-func Middleware(con *gorm.DB, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		nextCtx := app.StoreLoader(r.Context(), NewLoader(con))
-		r = r.WithContext(nextCtx)
-		next.ServeHTTP(w, r)
-	})
 }
